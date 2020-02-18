30db57652923bd4bd487222be11a7f9e9f13f1f7
Shiv Mohith <shivmohith8@gmail.com>
2020-02-18 09:25:04 +0530

    Delete all the files and starting from scratch

---
+++ ReadMe.txt
@@ -0,0 +1,14 @@
+node_modules contains all the dependencies
+    react and react-dom are the important ones
+    the name has to be "node_modules"
+    git repository does not contain node_modules
+    so when you clone react repository, you won't have node_modules, but you can use "npm install" to install dependencies based on package.json
+    
+all the static files are in public folder
+    any third-party javascript files can be placed in public folder
+
+package.json is the configuration files - all node.js files have package.json
+
+jest framework is used for testing
+
+"npm start" is used in dev environment 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+); /* first parameter is JSX and put/render the JSX(HTML) code inside the second parameter (div tag)
+      first parameter is JSX, not HTMl, though it looks like it. This JSX is converted (by React library) into 
+      React.createElement("div", 
+                           null, 
+                           React.createElement("h1", null, " Blogger App "), 
+                           React.createElement("p", null, " A simple blogging app in React! ")); */ 
+// ReactDOM.render(<h1> Blogger App </h1>, document.querySelector('#root')); // another way
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* Name of the function should be in Pascal case - start with capital letter so that it doesn't conflict with the standard HTML elements' name */
+/* Component */
+function Header() {
+    return <div> 
+                <h1> Blogger App </h1>
+                <p> A simple blogging app in React! </p>
+           </div>;
+}
+
+function App() {
+    return <div>
+        <Header />
+    </div>
+}
